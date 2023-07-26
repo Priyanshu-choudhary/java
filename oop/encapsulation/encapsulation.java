9c96d6573032d1b966b6dfad9cc68f9e6fd47beb
yadi choudhary <84007868+Priyanshu-choudhary@users.noreply.github.com>
2023-07-26 06:08:02 +0000

    encapluatiion

---
+++ oop/encapsulation/encapsulation.java
@@ -0,0 +1,10 @@
+ class Employee{
+    static int empId=2;
+     String empName="yadi";
+     int empSalery=1000;
+ public static void main (String[] args){
+    Employee opj = new  Employee();
+    empId=7;
+    System.out.println(empId);
+}
+}
