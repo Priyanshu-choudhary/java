c19a37e0e633b6a299619c58241cddbcbacfe0fb
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-05 22:54:04 +0530

    Sat  5 Aug 22:54:01 IST 2023

--- collectionFramWork/LinkList.java
+++ collectionFramWork/LinkList.java
@@ -3,7 +3,7 @@ class LinkList {
  public static void main(String[] args){
 	List list = new ArrayList();
 	//LinkList a = new LinkList();
-	list.add(23);
+	list.add(3,23);
 	System.out.print(list);
  }
 }
