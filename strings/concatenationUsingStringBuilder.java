fe1ce3190c7d007766613c696fe0305647de427c
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-10 21:36:05 +0530

    Thu 10 Aug 21:36:02 IST 2023

--- strings/concatenationUsingStringBuilder.java
+++ strings/concatenationUsingStringBuilder.java
@@ -3,8 +3,8 @@ public class StrBuilder
     /* Driver Code */  
     public static void main(String args[])  
     {  
-        StringBuilder s1 = "Hello";    //String 1  
-        StringBuilder s2 = " World";    //String 2  
+        StringBuilder s1 = new StringBuilder("Hello");    //String 1  
+        StringBuilder s2 = new StringBuilder(" World"); 
         StringBuilder s = s1.append(s2);   //String 3 to store the result  
         System.out.println(s.toString());  //Displays result  
     }  
