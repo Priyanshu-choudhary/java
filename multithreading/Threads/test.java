ab3fa8aa6917faad90fb47ec339ab85466d300d1
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-15 14:59:04 +0530

    Tue 15 Aug 14:59:01 IST 2023

--- multithreading/Threads/test.java
+++ multithreading/Threads/test.java
@@ -1,18 +1,19 @@
-class test1 extends Thtead{
+class test1 extends Thread{
 void run(){
  for(int i = 0;i<10;i++){
 	Thread.sleep(1000);
 	System.out.print(i);
 }
 }
-class test2 extends Thtead{
+}
+class test2 extends Thread{
 void run(){
  for(int i = 0;i<10;i++){
 	Thread.sleep(1000);
 	System.out.print(i);
 }
 }
-
+}
 class multitasking {
 public static void main (String args[]){
 	test1 t1 = new test1();
