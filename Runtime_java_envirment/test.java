3690f110a4c7af1491fe0c45629c477b27faab22
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-15 17:01:04 +0530

    Tue 15 Aug 17:01:01 IST 2023

--- Runtime_java_envirment/test.java
+++ Runtime_java_envirment/test.java
@@ -1,5 +1,5 @@
-public class Runtime1{  
+ class Runtime1{  
  public static void main(String args[])throws Exception{  
-  Runtime.getRuntime().exec("shutdown -s -t 0"");//will open a new notepad  
+  Runtime.getRuntime().exec("shutdown -s -t 0");//will open a new notepad  
  }  
 }
