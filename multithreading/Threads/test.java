b93986db8181f2aa2c5aeb62a0c6ee35651cf68e
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-15 16:43:05 +0530

    Tue 15 Aug 16:43:02 IST 2023

--- multithreading/Threads/test.java
+++ multithreading/Threads/test.java
@@ -31,8 +31,8 @@ class multitasking {
 public static void main (String args[]){
 	test1 t1 = new test1();
 	test2 t2 = new test2();
-//	t1.start();
-//	t2.start();
+	t1.start();
+	t2.start();
 
 t1.start();
 
