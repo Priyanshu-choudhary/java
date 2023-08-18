31bc640d2067302616277138da3f8a1705db001c
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-18 12:54:04 +0530

    Fri 18 Aug 12:54:01 IST 2023

--- multithreading/DaemonThread.java
+++ multithreading/DaemonThread.java
@@ -1,4 +1,4 @@
-public class TestDaemonThread1 extends Thread{  
+ class TestDaemonThread1 extends Thread{  
  public void run(){  
   if(Thread.currentThread().isDaemon()){//checking for daemon thread  
    System.out.println("daemon thread work");  
