4b8a767cc31ee02579a79d4e71a5823724927f91
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-18 13:48:05 +0530

    Fri 18 Aug 13:48:01 IST 2023

--- multithreading/ThreadGroup.java
+++ multithreading/ThreadGroup.java
@@ -1,9 +1,9 @@
-public class ThreadGroupDemo implements Runnable{    
+class ThreadGroup implements Runnable{    
     public void run() {    
           System.out.println(Thread.currentThread().getName());    
     }    
    public static void main(String[] args) {    
-      ThreadGroupDemo runnable = new ThreadGroupDemo();    
+      ThreadGroup runnable = new ThreadGroup();    
           ThreadGroup tg1 = new ThreadGroup("Parent ThreadGroup");    
               
           Thread t1 = new Thread(tg1, runnable,"one");    
