1e490d591f0db3d789fd52206f2e0e4886c02a07
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-18 12:27:05 +0530

    Fri 18 Aug 12:27:02 IST 2023

---
+++ multithreading/join.java
@@ -0,0 +1,24 @@
+import java.io.*;  
+class ThreadJoin extends Thread{
+public void run(){
+for (int j = 0; j < 2; j++){
+try{Thread.sleep(300);  
+System.out.println("The current thread name is: " + Thread.currentThread().getName());  }catch(Exception e){System.out.println("The exception has been caught: " + e);}
+System.out.println( j );  
+}}}
+public class ThreadJoinExample{
+public static void main (String argvs[]){
+ThreadJoin th1 = new ThreadJoin();
+ThreadJoin th2 = new ThreadJoin();
+
+th1.start();
+
+try{
+System.out.println("The current thread name is: "+ Thread.currentThread().getName());
+th1.join();
+}catch(Exception e){}
+
+th2.start();
+
+}
+}
