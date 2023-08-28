8c324b07d9597d63ea8901fd22d5cf218a340600
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-28 21:13:04 +0530

    Mon 28 Aug 21:13:01 IST 2023

--- assertion/assertionDemo.java
+++ assertion/assertionDemo.java
@@ -2,7 +2,7 @@
  class Example {  
     public static void main(String[] args)  
     {  
-        int age = 15;  
+        int age = 19;  
         // using assert keyword on two expressions  
         assert age <= 18 : " you cannot have a license ";  
         System.out.println(" Age of the person is " + age);  
