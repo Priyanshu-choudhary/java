a15be6e2071344aa48db1d1d38e4ebfe77de0e0c
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-28 21:12:04 +0530

    Mon 28 Aug 21:12:01 IST 2023

---
+++ assertion/assertionDemo.java
@@ -0,0 +1,10 @@
+// This is a program to demonstrate Assertions in Java.  
+ class Example {  
+    public static void main(String[] args)  
+    {  
+        int age = 15;  
+        // using assert keyword on two expressions  
+        assert age <= 18 : " you cannot have a license ";  
+        System.out.println(" Age of the person is " + age);  
+    }  
+}
