ee278e7ac3bdc489cec86b08e53477f27209148a
Priyanshu-choudhary <broyadi23@gmail.com>
2023-08-29 14:37:01 +0530

    Tue 29 Aug 14:37:01 IST 2023

--- generic/example.java
+++ generic/example.java
@@ -1,5 +1,9 @@
+class example{
+public static void main (Strings args[]){
 List<String> words = new ArrayList<String>();
 words.add("Hello ");
 words.add("world!");
 String s = words.get(0)+words.get(1);
 assert s.equals("Hello world!");
+}
+}
