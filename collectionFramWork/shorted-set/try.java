5b71309cf2a2545b29b9a57d70c490510403523d
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-07 20:07:16 +0530

    Thu  7 Sep 20:07:08 IST 2023

--- collectionFramWork/shorted-set/try.java
+++ collectionFramWork/shorted-set/try.java
@@ -2,12 +2,13 @@ import java.util.*;
  class Demo
 {
 	public static void main(String[] args){
-
-//	for(int i=0;i<args.length;i++){
-//	System.out.println(args[i]);
-//}
+/*
+	for(int i=0;i<args.length;i++){
+	System.out.println(args[i]);
+}*/
 	String fname =args[0];
 	String lname =args[1];
+	System.out.println(args.length);
 	System.out.println(fname.charAt(0));
 	System.out.println(lname);
 
