1017a09377a33014b8cdda98085850656314d25d
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-07 19:56:17 +0530

    Thu  7 Sep 19:56:09 IST 2023

--- collectionFramWork/shorted-set/try.java
+++ collectionFramWork/shorted-set/try.java
@@ -1,7 +1,7 @@
 import java.util.*;
  class Question
 {
-	public static void main(String[] args)
+	public static void main(String[] args){
 
 	for(int i=0;i<args.size;i++){
 	System.out.println(args[i]);
