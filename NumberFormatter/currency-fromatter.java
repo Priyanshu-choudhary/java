3c59af4412d5f704e89bd415b95e5efaaa04eb35
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 01:50:18 +0530

    Mon 11 Sep 01:50:10 IST 2023

---
+++ NumberFormatter/currency-fromatter.java
@@ -0,0 +1,25 @@
+
+// Java program to implement
+// the above function
+  
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Currency;
+  
+public class Main {
+    public static void main(String[] args)
+        throws Exception
+    {
+  
+        NumberFormat nF = NumberFormat.getNumberInstance();
+  
+        // Initially currency
+        System.out.println("Initially Currency: "+ nF.getCurrency());
+  
+        // Currency set to US
+        nF.setCurrency(Currency.getInstance(Locale.CANADA));
+  
+        // Print the currency
+        System.out.println("Currency set as: "+ nF.getCurrency());
+    }
+}
