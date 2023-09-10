736db3d08922e57084e5b88008f5c43d8f2614e0
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 00:36:17 +0530

    Mon 11 Sep 00:36:09 IST 2023

--- DATE&Timejava/date/operationOnDate.java
+++ DATE&Timejava/date/operationOnDate.java
@@ -6,5 +6,9 @@ class LocalDateExample2 {
     System.out.println("Is 2017 1 13 date is leap year or not    :"+date1.isLeapYear());    
     LocalDate date2 = LocalDate.of(2016, 9, 23);    
     System.out.println("Is 2016 9 23 date is leap year or not    :"+date2.isLeapYear());    
-  }    
+	
+	boolean a = date1.after(date2);
+	 System.out.println("Date d1 comes after " +
+                           "date d2: " + a);  
+}    
 }  
