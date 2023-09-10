af0bf64d05044c4967819ca6e98def723dd04964
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 00:37:16 +0530

    Mon 11 Sep 00:37:08 IST 2023

--- DATE&Timejava/date/operationOnDate.java
+++ DATE&Timejava/date/operationOnDate.java
@@ -2,8 +2,9 @@ import java.time.LocalDate;
 class LocalDateExample2 {    
   public static void main(String[] args) { 
 	   
-    LocalDate date1 = LocalDate.of(2017, 1, 13);    
-    System.out.println("Is 2017 1 13 date is leap year or not    :"+date1.isLeapYear());    
+    Date date1 = Date.of(2017, 1, 13);    
+    
+	System.out.println("Is 2017 1 13 date is leap year or not    :"+date1.isLeapYear());    
     LocalDate date2 = LocalDate.of(2016, 9, 23);    
     System.out.println("Is 2016 9 23 date is leap year or not    :"+date2.isLeapYear());    
 	
