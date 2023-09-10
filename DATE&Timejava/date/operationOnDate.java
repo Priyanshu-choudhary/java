29512995dfbec08875db79dbac9c5ab08125c1e4
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 00:00:18 +0530

    Mon 11 Sep 00:00:09 IST 2023

--- DATE&Timejava/date/operationOnDate.java
+++ DATE&Timejava/date/operationOnDate.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;    
-public class LocalDateExample2 {    
+class LocalDateExample2 {    
   public static void main(String[] args) { 
 	   
     LocalDate date1 = LocalDate.of(2017, 1, 13);    
