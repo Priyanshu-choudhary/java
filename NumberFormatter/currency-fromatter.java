69dad1dbc84292e88790513d8a41f8c7a3e6296b
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 02:30:16 +0530

    Mon 11 Sep 02:30:08 IST 2023

--- NumberFormatter/currency-fromatter.java
+++ NumberFormatter/currency-fromatter.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+class Solution {
     
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
