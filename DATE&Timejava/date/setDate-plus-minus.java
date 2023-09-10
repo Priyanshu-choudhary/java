66d2742c8b9386375742c3d4575cb1931c28b244
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 00:27:17 +0530

    Mon 11 Sep 00:27:09 IST 2023

---
+++ DATE&Timejava/date/setDate-plus-minus.java
@@ -0,0 +1,11 @@
+mport java.time.LocalTime;  
+class LocalTimeExample6 {  
+  public static void main(String[] args) {  
+    LocalDate d = LocalDate.of(2020,07,22);  
+    System.out.println(d);  
+    LocalDate d2 =d.minusDays(2);
+    LocalDate d2 =d.minusMounths(5);  
+    LocalDate d2 =d.minusYears(5);
+    System.out.println(d2);  
+  }  
+}  
