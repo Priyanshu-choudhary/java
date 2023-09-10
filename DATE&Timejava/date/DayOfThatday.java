5bed8e3c14822e75fdd8f04d76d238141283b1f7
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 01:14:17 +0530

    Mon 11 Sep 01:14:08 IST 2023

---
+++ DATE&Timejava/date/DayOfThatday.java
@@ -0,0 +1,14 @@
+import java.time.*;  
+import java.time.DayOfWeek;  
+    
+class DayOfWeekExample5  
+{  
+    public static void main(String ar[])  
+    {  
+        LocalDate localDate = LocalDate.of(2021, Month.SEPTEMBER, 13);  
+        DayOfWeek dayOfWeek = DayOfWeek.from(localDate);  
+        System.out.println("Day of the Week on" + " 13th September 2021 - " + dayOfWeek.name());  
+        int val = dayOfWeek.getValue();  
+        System.out.println("Int Value of " + dayOfWeek.name() + " - " + val);  
+    }  
+}  
