81bcacad336b6e7a256de2254b8852e7a52ce7f0
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-10 21:54:17 +0530

    Sun 10 Sep 21:54:09 IST 2023

--- Runtime_java_envirment/test.java
+++ Runtime_java_envirment/test.java
@@ -1,5 +1,5 @@
  class Runtime1{  
  public static void main(String args[])throws Exception{  
-  Runtime.getRuntime().exec("shutdown ");//will open a new notepad  
+  Runtime.getRuntime().exec("cd ~/java && mkdir createForTesting && cd createForTesting && touch test.txt");//will open a new notepad  
  }  
 }
