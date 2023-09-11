6821b78d0c6c449b6e2f0b2389aebae767c1e768
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 21:25:17 +0530

    Mon 11 Sep 21:25:09 IST 2023

---
+++ strings/palindromeString.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+     Scanner sc=new Scanner(System.in);
+     String A=sc.next();
+	 int lc=0;
+	 if(A.length()%2==0){lc=A.length()/2;}
+	 else{lc=A.length()-1/2;}
+	
+	 for(int i=0;i<lc;i++){
+		if(A.charAt(i)==A.charAt(A.length()-i))
+			System.out.printf("  T ");
+		}else{
+			System.out.printf("  F ");	
+			}
+	}
+}
