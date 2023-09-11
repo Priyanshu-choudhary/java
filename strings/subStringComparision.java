0366e7cc75627d5e818a821bdcdc8853d447d6d3
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 20:47:20 +0530

    Mon 11 Sep 20:47:10 IST 2023

--- strings/subStringComparision.java
+++ strings/subStringComparision.java
@@ -5,43 +5,13 @@ class Solution {
 public static void main(String args[]){ 
          NavigableSet<String> ns = new TreeSet<>();       
 
-		String smallest = "";
-        String largest = "";
-        String s="welcometojava";
-		String sub="";
-		int k=3;
-		int rg=1;
-		int small=1;
-        // Complete the function
-        // 'smallest' must be the lexicographically smallest substring of length 'k'
-        // 'largest' must be the lexicographically largest substring of length 'k'
         for(int i=0;i<=s.length()-k;i++){
-            sub= s.substring(i,i+k);
-            //System.out.println(sub);
+        	String sub= s.substring(i,i+k);
             ns.add(sub);
-          /*  for(int j=0;j<=s.length()-k-1;j++){
-                String subn=s.substring(j+1,j+k+1);
-                //System.out.print("subn "+subn);
-				      
-            	//System.out.println("   check  "+sub.compareTo(subn));
-				if(sub.compareTo(subn)>0){
-					small=0;
-				}else{}
-				if(sub.compareTo(subn)<0){
-					rg=0;
-				}else{}
-			}
-			if(rg==1){System.out.println("  greatest=   "+sub);}	
-			if(small==1){System.out.println("  smallest=   "+sub);}
-        	//System.out.println("small "+small+" greatest "+rg);
-			rg=1;
-			small=1;
-		
-
-*/
 		}
-        System.out.println(ns);
- 		System.out.println(ns.pollLast());
-		System.out.println(ns.pollFirst());
+        
+		String smallest = ns.pollFirst();
+        String largest = ns.pollLast();
+	
   }
 }
