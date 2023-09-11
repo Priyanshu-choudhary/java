1c0bef0dd6809219ab03f177308498808387bc9a
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 23:51:18 +0530

    Mon 11 Sep 23:51:10 IST 2023

--- strings/anagramsString.java
+++ strings/anagramsString.java
@@ -6,6 +6,7 @@ class Solution {
         // Complete the function
 		int frequency=0;
         Map<Character,Integer> tm =new TreeMap<Character,Integer>();
+		Map<Character,Integer> tm2 =new TreeMap<Character,Integer>();
 
 		for(int i=0;i<a.length();i++){
 			for(int j=0;j<a.length();j++){
@@ -16,7 +17,18 @@ class Solution {
 				}
 			frequency=0;
 			}
+		
+		for(int i2=0;i2<b.length();i2++){
+			for(int j2=0;j2<b.length();j2++){
+				if(b.charAt(i2)==b.charAt(j2)){
+					frequency++;
+					tm2.put(b.charAt(i2),frequency);
+					}
+				}
+			frequency=0;
+			}
 		System.out.println(tm);
+		System.out.println(tm2);
         return true;
     }
 
