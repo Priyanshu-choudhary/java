65e7d72e2e1431f1f03382d45cd745628508f234
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 21:31:17 +0530

    Mon 11 Sep 21:31:09 IST 2023

--- strings/palindromeString.java
+++ strings/palindromeString.java
@@ -9,7 +9,7 @@ class Solution {
      String A=sc.next();
 	 int lc=0;
 	 if(A.length()%2==0){lc=A.length()/2;}
-	 else{lc=A.length()-1/2;}
+	 else{lc=(A.length()-1)/2;}
 	 System.out.println(lc);
 	 for(int i=0;i<lc;i++){
 		if(A.charAt(i)==A.charAt(A.length()-i)){
