2e5c0c4282789fae4c8dc4043d0b2e7c11ae6b45
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 21:01:16 +0530

    Mon 11 Sep 21:01:08 IST 2023

--- strings/subStringComparision.java
+++ strings/subStringComparision.java
@@ -1,17 +1,24 @@
+import java.io.*;
 import java.util.*;
-
-class Solution {
-
-public static void main(String args[]){ 
-         NavigableSet<String> ns = new TreeSet<>();       
-
-        for(int i=0;i<=s.length()-k;i++){
-        	String sub= s.substring(i,i+k);
-            ns.add(sub);
-		}
+public class Solution {
+    public static String getSmallestAndLargest(String s, int k) {
+         NavigableSet<String> ns = new TreeSet<>();
         
-		String smallest = ns.pollFirst();
-        String largest = ns.pollLast();
-	
-  }
+         for(int i=0;i<=s.length()-k;i++){
+              String sub= s.substring(i,i+k);
+               ns.add(sub);
+            }
+             String largest = ns.pollLast(); 
+             String smallest = ns.pollFirst();  
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
 }
