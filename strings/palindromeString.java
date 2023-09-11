9885f6e55c73d91ae1b81d58ca05f3bfce98b03e
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 21:39:17 +0530

    Mon 11 Sep 21:39:09 IST 2023

--- strings/palindromeString.java
+++ strings/palindromeString.java
@@ -8,16 +8,24 @@ class Solution {
      Scanner sc=new Scanner(System.in);
      String A=sc.next();
 	 int lc=0;
+	 int res=1;
 	 if(A.length()%2==0){lc=A.length()/2;}
 	 else{lc=(A.length()-1)/2;}
 	 System.out.println(lc);
 	 
 	for(int i=0;i<lc;i++){
 		if(A.charAt(i)==A.charAt(A.length()-i-1)){
-			System.out.printf("  T ");
+			System.out.print("  T ");
+			
 		}else{
-			System.out.printf("  F ");	
+			System.out.print("  F ");
+			res=0;	
 			}
-		}
+		}if(res==0){
+			System.out.print("NO");
+			}else{
+				System.out.print("YES");
+				}
 	}
+	
 }
