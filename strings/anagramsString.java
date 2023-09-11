136a78b8073644a97006d37a995d48323969c94d
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 23:45:21 +0530

    Mon 11 Sep 23:45:13 IST 2023

--- strings/anagramsString.java
+++ strings/anagramsString.java
@@ -5,7 +5,7 @@ class Solution {
     static boolean isAnagram(String a, String b) {
         // Complete the function
 		int frequency=0;
-        Map<char,Integer> tm =new TreeMap<char,Integer>();
+        Map<Character,Integer> tm =new TreeMap<Character,Integer>();
 
 		for(int i=0;i<a.length();i++){
 			for(int j=0;j<a.length();j++){
