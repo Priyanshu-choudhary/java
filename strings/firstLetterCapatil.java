7629defb2626b014375cb3b8f93ad575926094f6
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 15:11:19 +0530

    Mon 11 Sep 15:11:10 IST 2023

---
+++ strings/firstLetterCapatil.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter two string");
+        
+		String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println("length sum"+A.length()+B.length());
+        if(A.compareTo(B)<=0){
+            System.out.println("first string come first in dictonary");
+        }else{
+            System.out.println("secound string come first in dictonary");
+        }
+        String s=A.substring(0,1).toUpperCase();
+        String s1=A.substring(1);
+        String sr=s;
+        sr=sr.concat(s1);
+        System.out.print(sr+" ");
+        
+         s=B.substring(0,1).toUpperCase();
+         s1=B.substring(1);
+         sr=s;
+        sr=sr.concat(s1);
+        System.out.print(sr);
+        
+    }
+}
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length()+B.length());
+        if(A.compareTo(B)<=0){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+        String s=A.substring(0,1).toUpperCase();
+        String s1=A.substring(1);
+        String sr=s;
+        sr=sr.concat(s1);
+        System.out.print(sr+" ");
+        
+         s=B.substring(0,1).toUpperCase();
+         s1=B.substring(1);
+         sr=s;
+        sr=sr.concat(s1);
+        System.out.print(sr);
+        
+    }
+}
+
+
+
