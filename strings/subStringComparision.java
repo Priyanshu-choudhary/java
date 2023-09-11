ef8904ee5294925652c846191ae3f407b19f98bc
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-11 17:05:26 +0530

    Mon 11 Sep 17:05:14 IST 2023

--- strings/subStringComparision.java
+++ strings/subStringComparision.java
@@ -26,12 +26,14 @@ public static void main(String args[]){
 				}
 				if(sub.compareTo(subn)<0){
 					rg=0;
-					System.out.println("  value changed   ");
+					//System.out.println("  value changed   ");
 				}
 			}
 			if(rg==1){System.out.println("  greatest=   "+sub);}	
 			if(small==1){System.out.println("  smallest=   "+sub);}
-        }
+        	int rg=0;
+			int small=0;
+		}
         
  
   }
