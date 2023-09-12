661f779828138cba035aefdec11a0e737f857114
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-12 21:25:16 +0530

    Tue 12 Sep 21:25:08 IST 2023

--- strings/tokenizer.java
+++ strings/tokenizer.java
@@ -2,8 +2,13 @@ import java.util.*;
 class Simple{  
  public static void main(String args[]){  
    StringTokenizer st = new StringTokenizer("my name is khan");  
-     while (st.hasMoreTokens()) {  
+   String s="Hello everyone I am a Java developer";
+   StringTokenizer st2=s;  
+	while (st.hasMoreTokens()) {  
          System.out.println(st.nextToken());  
      }  
+	 while (st2.hasMoreTokens()) {
+         System.out.println(st2.nextToken());       
+	    }   
    }  
 } 
