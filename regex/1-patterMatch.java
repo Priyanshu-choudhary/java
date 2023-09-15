818de59f1bdb9e297eed4749dd83c280d34fdff4
Priyanshu-choudhary <broyadi23@gmail.com>
2023-09-15 12:16:17 +0530

    Fri 15 Sep 12:16:09 IST 2023

--- regex/1-patterMatch.java
+++ regex/1-patterMatch.java
@@ -10,16 +10,7 @@ class GFG {
 	// Main driver method
 	public static void main(String args[])
 	{
-
-		// Following line prints "true" because the whole
-		// text "geeksforgeeks" matches pattern
-		// "geeksforge*ks"
-		System.out.println(Pattern.matches(
-			"geeksforge*ks", "geeksforgeeks"));
-
-		// Following line prints "false" because the whole
-		// text "geeksfor" doesn't match pattern "g*geeks*"
-		System.out.println(
-			Pattern.matches("g*geeks*", "geeksfor"));
+		System.out.println(Pattern.matches("geeksforge*ks", "geeksforgeeks"));
+		System.out.println(Pattern.matches("g*geeks*", "geeksfor"));
 	}
 }
